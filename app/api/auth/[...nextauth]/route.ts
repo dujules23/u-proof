454c7eab0afecf29181393a0e333f2125c2acc41
Durrell Jules <dujules23@gmail.com>
2025-01-24 00:37:39 -0500

    updated next auth route

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,7 +1,7 @@
 import NextAuth from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
 
-export const authOptions = {
+const handler = NextAuth({
   secret: process.env.NEXTAUTH_SECRET,
   providers: [
     GoogleProvider({
@@ -9,6 +9,7 @@ export const authOptions = {
       clientSecret: process.env.GOOGLE_CLIENT_SECRET || "",
     }),
   ],
-};
+});
 
-export default NextAuth(authOptions);
+export const GET = handler;
+export const POST = handler;
